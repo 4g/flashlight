c93b87d1e3f0db48622bb49ae9cff642acb752d5
sherin thomas <sherinct@live.com>
2017-10-31 11:39:09 +0530

    experimenting on nodes and inputs

--- test/visualizer.py
+++ test/visualizer.py
@@ -18,10 +18,17 @@ class Visualizer:
         self.net = net
         self.regx_op = re.compile(r'\^[a-zA-Z]+')
 
-    def round_a_loop(self, x):
+    def walk_around(self, x):
         sys.settrace(self._global_trace)
         trace, out = torch.jit.trace(self.net, x)
-        self._terminate_that_loop()
+        self._got_tired()
+        graph = trace.graph()
+        inputs = list(graph.inputs())
+        nodes = list(graph.nodes())
+        # print(inputs)
+        # print(nodes)
+        # exit()
+
         nodes = trace.graph().nodes()
         for node in nodes:
             val = self.regx_op.search(str(node))
@@ -61,7 +68,7 @@ class Visualizer:
                     filename, class_name, lineno, linecache.getline(filename, lineno)))
         return self._local_trace
 
-    def _terminate_that_loop(self):
+    def _got_tired(self):
         sys.settrace(None)
         # TODO - change the filename
         with open('traces/{}.json'.format(time.time()), 'w+') as f:
@@ -69,3 +76,27 @@ class Visualizer:
         print(self.torch_trace)
         self.py_trace = []
         self.torch_trace = []
+
+"""
+graph(%1 : Float(64, 10)
+      %2 : Float(100, 10)
+      %3 : Float(100)
+      %4 : Float(4, 100)
+      %5 : Float(4)) {
+  %7 : Float(10!, 100!) = ^Transpose(0, 1)(%2), uses = [[%8.i2]];
+  %9 : Float(64, 100), %10 : Handle = ^Addmm(1, 1, False)(%3, %1, %7), uses = [[%11.i0], []];
+  %12 : Float(64, 100) = ^Sigmoid()(%9), uses = [[%15.i1]];
+  %14 : Float(100!, 4!) = ^Transpose(0, 1)(%4), uses = [[%15.i2]];
+  %16 : Float(64, 4), %17 : Handle = ^Addmm(1, 1, False)(%5, %12, %14), uses = [[%18.i0], []];
+  %19 : Float(64, 4) = ^Sigmoid()(%16), uses = [[%0.i0]];
+  return (%19);
+}
+
+
+[%1 : Float(64, 10) = Param(), uses = [%8.i1];
+, %2 : Float(100, 10) = Param(), uses = [%6.i0];
+, %3 : Float(100) = Param(), uses = [%8.i0];
+, %4 : Float(4, 100) = Param(), uses = [%13.i0];
+, %5 : Float(4) = Param(), uses = [%15.i0];
+]
+"""
