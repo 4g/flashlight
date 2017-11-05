4180d1d0f653189361494bafe7e4931379d0de12
sherin thomas <sherinct@live.com>
2017-11-05 22:53:26 +0530

    trace format change

--- test/visualizer.py
+++ test/visualizer.py
@@ -7,6 +7,9 @@ import re
 import torch
 
 
+possible_nodes = ['transpose', 'sigmoid']
+
+
 class Visualizer:
     """
     Base visualization class
@@ -23,17 +26,13 @@ class Visualizer:
         trace, out = torch.jit.trace(self.net, x)
         self._got_tired()
         graph = trace.graph()
+        print(graph)
         inputs = list(graph.inputs())
         nodes = list(graph.nodes())
-        # print(inputs)
-        # print(nodes)
-        # exit()
-
-        nodes = trace.graph().nodes()
         for node in nodes:
-            val = self.regx_op.search(str(node))
-            if val:
-                self.torch_trace.append(val.group()[1:])
+            user = node.uses()[0].user.unique()
+            whattype = node.type().scalarType()
+            size = node.type().sizes()
 
     def _global_trace(self, frame, why, arg):
         if why == 'call':
@@ -61,7 +60,7 @@ class Visualizer:
             if class_obj is not None:
                 class_name = class_obj.__class__.__name__
             else:
-                class_name = ''
+                class_name = '<NO_CLASS_NAME>'
             # TODO - remove line, use only line no
             self.py_trace.append(
                 "File: {}, class: {}, Line: {} {}".format(
@@ -73,30 +72,5 @@ class Visualizer:
         # TODO - change the filename
         with open('traces/{}.json'.format(time.time()), 'w+') as f:
             json.dump({'py_trace': self.py_trace, 'torch_trace': self.torch_trace}, f, indent=2)
-        print(self.torch_trace)
         self.py_trace = []
         self.torch_trace = []
-
-"""
-graph(%1 : Float(64, 10)
-      %2 : Float(100, 10)
-      %3 : Float(100)
-      %4 : Float(4, 100)
-      %5 : Float(4)) {
-  %7 : Float(10!, 100!) = ^Transpose(0, 1)(%2), uses = [[%8.i2]];
-  %9 : Float(64, 100), %10 : Handle = ^Addmm(1, 1, False)(%3, %1, %7), uses = [[%11.i0], []];
-  %12 : Float(64, 100) = ^Sigmoid()(%9), uses = [[%15.i1]];
-  %14 : Float(100!, 4!) = ^Transpose(0, 1)(%4), uses = [[%15.i2]];
-  %16 : Float(64, 4), %17 : Handle = ^Addmm(1, 1, False)(%5, %12, %14), uses = [[%18.i0], []];
-  %19 : Float(64, 4) = ^Sigmoid()(%16), uses = [[%0.i0]];
-  return (%19);
-}
-
-
-[%1 : Float(64, 10) = Param(), uses = [%8.i1];
-, %2 : Float(100, 10) = Param(), uses = [%6.i0];
-, %3 : Float(100) = Param(), uses = [%8.i0];
-, %4 : Float(4, 100) = Param(), uses = [%13.i0];
-, %5 : Float(4) = Param(), uses = [%15.i0];
-]
-"""
