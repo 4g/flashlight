6f2020c58e8648ec9f9f2747ad6ddeb52d409f61
sherin thomas <sherinct@live.com>
2017-12-22 14:34:39 +0530

    python tracer

--- lucent/client/python_tracer.py
+++ lucent/client/python_tracer.py
@@ -18,21 +18,20 @@ class PythonTracer:
         sys.settrace(None)
 
     def _trace_callback(self, frame, why, arg):
-        if why == 'call':
-            code = frame.f_code
-            filename = frame.f_globals.get('__file__', None)
-            if filename:
-                self.trace.append((filename, code.co_name, '======================='))
-            else:
-                print('Returning None')
-                return None
-        if why == "line":
-            filename = frame.f_code.co_filename
-            lineno = frame.f_lineno
-            class_obj = frame.f_locals.get("self", None)
-            if class_obj is not None:
-                class_name = class_obj.__class__.__name__
-            else:
-                class_name = '<NO_CLASS_NAME>'
-            self.trace.append((filename, class_name, lineno, linecache.getline(filename, lineno)))
+        lineno = frame.f_lineno
+        filename = frame.f_code.co_filename
+        if '/torch/' not in filename and 'lucent/client/' not in filename:
+            if why == 'call':
+                code = frame.f_code
+                if filename:
+                    self.trace.append((filename, code.co_name, linecache.getline(filename, lineno), '======================='))
+                else:
+                    raise Exception('Filename is empty')
+            if why == "line":
+                class_obj = frame.f_locals.get("self", None)
+                if class_obj is not None:
+                    class_name = class_obj.__class__.__name__
+                else:
+                    class_name = '<NO_CLASS_NAME>'
+                self.trace.append((filename, class_name, lineno, linecache.getline(filename, lineno)))
         return self._trace_callback
