2f80c11316d2bd01474738f77e320f43304ec397
hhsecond <sherinct@live.com>
2018-02-09 15:42:27 +0530

    Sanic izing the server

--- flashlight/backend/server.py
+++ flashlight/backend/server.py
@@ -1,7 +1,12 @@
 from sanic import Sanic
 from sanic.response import json
 
+
 app = Sanic()
+app.static('/', '../frontend/build')
+app.static('/static', '../frontend/build/static')
+app.static('/static/js', '../frontend/build/static/js')
+app.static('/index', '../frontend/build/index.html')
 
 
 @app.route("/")
@@ -9,5 +14,9 @@ async def test(request):
     return json({"hello": "world"})
 
 
-def run():
-    app.run(host="0.0.0.0", port=8000)
+def run(debug=False):
+    app.run(host="0.0.0.0", port=8000, debug=debug)
+
+
+if __name__ == '__main__':
+    run()
