f83d33392ed57a1da547c32b2c10f521f99ebdcf
hhsecond <sherinct@live.com>
2018-02-11 11:12:34 +0530

    statusbus

--- flashlight/backend/utility.py
+++ flashlight/backend/utility.py
@@ -16,13 +16,51 @@ def compact_files(folder, outfile):
             try:
                 full.append(json.load(f))
             except json.decoder.JSONDecodeError:
-                pass
+                print('FlashlightHome has files those are not JSON friendly, exiting..')
+                return False
 
-    # doing another loop since removing the file in previous loop could
-    # cause data lose if user stops the process in between
-    for file in os.listdir(folder):
-        os.remove(os.path.join(folder, file))
+    try:
+        # doing another loop since removing the file in previous loop could
+        # cause data lose if user stops the process in between
+        for file in os.listdir(folder):
+            os.remove(os.path.join(folder, file))
 
-    # writing outfile
-    with open(outfile, 'w+') as f:
-        json.dump(full, f)
+        # writing outfile
+        with open(outfile, 'w+') as f:
+            json.dump(full, f)
+    except PermissionError:
+        print("FlashLight doesn't have permission to use it's own home, exiting...")
+        return False
+
+    return True
+
+
+def init_homefolder(folder, outfile):
+    try:
+        os.makedirs(folder)
+        with open(outfile, 'w+') as f:
+            json.dump([], f)
+    except PermissionError:
+        print("FlashLight doesn't have permission to use it's own home, exiting...")
+        return False
+
+
+class StatusBus:
+    """ Channel that keeps the status from different fucntions """
+
+    def __init__(self):
+        self._status = []
+
+    @property
+    def status(self):
+        return all(self._status)
+
+    @status.setter
+    def status(self, val):
+        if isinstance(val, bool):
+            self._status.append(val)
+        else:
+            raise Exception('Cannot add non-bool value to status bus')
+
+    def clear(self):
+        self._status = []
